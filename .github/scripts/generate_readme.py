55588add08cf953e10a849507892b664cdad9d97
那位路人 <gebaocai@gmail.com>
2025-09-29 03:27:38 +0000

    update

--- .github/scripts/generate_readme.py
+++ .github/scripts/generate_readme.py
@@ -39,6 +39,7 @@ def generate_readme(dns_file="ziyongdns", ruler_file="ziyongruler", readme_file=
 ```
 
 一个github能用的脚本或者工作流,用于合并Adguard的过滤规则,思路如下:合并自定义的Adguard官方规则,并将其存储在AdguardDNSRuler和Adguardruler,合并列表自定义第三方规则,并将其合并在ziyongdnsZ和ziyongrulerZ,如果AdguardDNSRuler和ziyongdnsZ中有相同规则,则删除ziyongdnsZ中的相同规则,并将规则存储到ziyongdnsZ1中,同理处理Adguardruler和ziyongrulerZ的规则,井存储到ziyongrulerZ1,最后对ziyongdnsZ1和ziyongrulerZ1进行优化,查看ziyongdnsZ1的规则中是否有包含关系的规则,如果有,则删除被包含的规则,并存储到ziyongdns中,同理处理ziyongrulerZ1,然后处理后存储到ziyongruler
+dns增减检测机制, 有效域名每180天检测一次，无效域名每7天检测一次持续4次无效者180天再次检测
     """
 
     with open(readme_file, 'w') as f:
